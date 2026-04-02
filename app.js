91725a8262dd9cbcb6dc593625135268242e3355
LWalk12 <S551736@nwmissouri.edu>
2026-04-01 22:16:07 -0500

    part 7: made grid pug and js, made endpoint, and made it write to the console

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var technologyRouter = require('./routes/technology');
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/technology', technologyRouter);
+app.use('/grid', gridRouter);
 
 
 // catch 404 and forward to error handler
