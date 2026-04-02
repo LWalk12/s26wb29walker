35653dd12a2ddde9060734b5f60e52a8b377bcd6
LWalk12 <S551736@nwmissouri.edu>
2026-04-01 22:43:29 -0500

    part 9: added new endpoint as well as reference to the other pages on index.pug

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var technologyRouter = require('./routes/technology');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/technology', technologyRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 
 
 // catch 404 and forward to error handler
